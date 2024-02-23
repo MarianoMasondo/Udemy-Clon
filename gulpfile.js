af98da21fd4c83156792386086856c9d40eecdb9
Mariano Masondo <nanomason@gmail.com>
2024-02-23 18:06:19 -0300

    Start 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const { src, dest, watch, series } = require('gulp');
 
 // Compilar CSS
-
 const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
 const rename = require('gulp-rename');
